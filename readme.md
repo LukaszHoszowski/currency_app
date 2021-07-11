dcf97d115f067253d142f0f8e0c22fc424166ad5
Lukasz Hoszowski <hoszowski@hpe.com>
2021-07-11 16:54:48 +0200

    Finished Currencies

--- readme.md
+++ readme.md
@@ -1,31 +1,87 @@
 # Currency app
 
+## The story of asynchronous technic in js
 
-## The story  of asynchronous technic in js
 1. Callback - callback hell
-2. Promise(Future w pythonie) - 
+2. Promise(Future w pythonie) -
 3. Generatory
 4. Async/Await
 
 ## Dictionary
+
 - AJAX - asynchronous javascript and xml
 - SPA - Single page application
 
+## REST api (alternative GraphQL)
+
+- CRUD
+
+|  CRUD   |  SQL   |   REST    |
+| :-----: | :----: | :-------: |
+| create  | INSERT |   POST    |
+|  ready  | SELECT |    GET    |
+| upgrade | UPDATE | PUT/PATCH |
+| delete  | DELETE |  DELETE   |
+
+- endpoint
+
+1. GET -> domain/books - wiele ksiazek
+2. GET -> domain/books/<id> - konkretna ksiazka
+3. POST -> domain/books
+4. PUT/PATCH -> domain/books/<id>
+5. DELETE - domain/books/<id>
+
+- users
+
+1. GET -> /users
+2. GET -> /users/<id>
+3. POST -> /users
+4. PUT/PATCH -> /users/<id>
+5. DELETE -> /users/<id>
+
+<!-- /users/<id>/products/<id> -->
+
+## HTTP
+- status code
+- headers
+- body
+
+```
+https://www.maps.google.com/city?elo=xd&yolo=2
+```
+
+1. https:// -> protocol (ftp, ssh, pop3, smtp, imap, ws)
+2. www -> type (file, pdf)
+3. maps -> subdomain
+4. google -> domain
+5. com -> domain family (eu, tv - tuval)
+6. city - endpoint/source
+7. ? -> start parameters (fbclid)
+8. elo=xd - param, key value
+9. & - param separator
+
+btoa() -> konwertowanie do base64
+atob() -> konwertowanie z base64
+
+Axios - client do robienia requestow
+
+---------------------------------------------------------------------------
 ## Variables
+
         Scope       Reinicialization    Redeclaration   Hoisting
 
-VAR     Func        Y                   Y               Y
+VAR Func Y Y Y
 
-LET     Block       Y                   X               X
+LET Block Y X X
 
-Const   Block       X                   X               X
+Const Block X X X
 
 Hoisting - zmienne VAR, funkcje, classy
 
 Funkcje
 
-Named                                   Annonymous
-Function name (param) {body return}     function (param) {body return}
+Named Annonymous
+Function name (param) {body return} function (param) {body return}
 
 Dzialanie funkcjo zalezne od kontekstu
 
@@ -33,13 +89,13 @@ fn arrow
 Explicite return
 () => {return 1}
 Implicite return
-x => x * 2 (ukyty return)
-(x, y) => x * y
+x => x _ 2 (ukyty return)
+(x, y) => x _ y
 
 const d = () => ({"ala": 2}) uzyc okraglych nawiasow aby uzyskac implicite return
 
-Call stack  FILO
-queue       FIFO
+Call stack FILO
+queue FIFO
 
 deque w pythonie - kolejka dwustronna
 
@@ -57,4 +113,10 @@ Promise -> ES6
 Generator -> ES7
 Async/Await -> ES7
 
-Promise (jest funkcjonalnosc w jezyku pozwalajaca obslugiwac async) - zwraca obiekt od razu, jesli sie uda to resolve()->.then()|reject().catch(), 
+Promise (jest funkcjonalnosc w jezyku pozwalajaca obslugiwac async) - zwraca obiekt od razu, jesli sie uda to resolve()->.then()|reject().catch(),
+
+
+enctype="multipart/form-data" - wysylanie zdjec, zdjecie za duze zeby wyslac w jednej ramce, serwer musi wiedziec ze ma czekac na pozostale ramki
+
+
+moment js do obslugi data
--- src/app.mjs
+++ src/app.mjs
@@ -1,74 +1,119 @@
-import codes from "./codes.mjs"
+import codes from './codes.mjs';
+import { postData, getData, removeData } from './currency_crud.mjs';
 
-const currencyCodesRef = document.querySelector("#currencyCodesRef");
-const btnCheckCurrencyRef = document.querySelector("#checkCurrency")
-const resultRef = document.querySelector("#resultRef");
+const currencyCodesRef = document.querySelector('#currencyCodesRef');
+const btnCheckCurrencyRef = document.querySelector('#checkCurrency');
+const resultRef = document.querySelector('#resultRef');
 const cardRef = document.querySelector('#cardRef');
-const alertMessageRef = document.querySelector("#alert-message")
+const alertMessageRef = document.querySelector('#alertMessage');
+const listItemtRef = document.querySelector('#listItemtRef');
+const listRef = document.querySelector('#listRef');
 
-
-
-codes.forEach(code => {
-    const optTag = document.createElement('option');
-    const optText = document.createTextNode(code);
-    optTag.appendChild(optText);
-
-    optTag.setAttribute("value", code);
-    currencyCodesRef.appendChild(optTag);
-})
-
-
-const getExchangeRate = async (code) => {
-    const url = 'http://api.nbp.pl/api/exchangerates/rates/a/'
-    return fetch(`${url}${code}/`)
-        .then((response) => {
-            if (response.status == 200) {
-                return response.json()
-            } else {
-                return response.text()
-            }
-        }).catch((e) => e);
-
-};
-
-// const result = await getExchangeRate("HRK");
-// console.log(result);
-
-// btnCheckCurrencyRef.addEventListener("click", function (event) {
-//     event.preventDefault();
-//     console.log(this)
-// })
 String.prototype.capitalize = function () {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
+codes.forEach((code) => {
+  const optTag = document.createElement('option');
+  const optText = document.createTextNode(code);
+  optTag.appendChild(optText);
+
+  optTag.setAttribute('value', code);
+  currencyCodesRef.appendChild(optTag);
+});
+
+const deleteItem = (evt) => {
+    const item = evt.target.parentElement
+    const id = item.getAttribute("data-id")
+    removeData(`http://localhost:3000/currencies/${id}`)
+    item.remove();
+    
+}
+
+const createElement = (data) => {
+    const itemRef = listItemtRef.cloneNode(true);
+    
+    itemRef.setAttribute("data-id", data.id)
+
+    const badge = itemRef.querySelector('.badge');
+    const code = itemRef.querySelector('.fw-bold');
+    const btnDelete = itemRef.querySelector('.btn-danger');
+    const rate = code.nextElementSibling;
+    badge.innerText = moment(data.date).fromNow();
+    code.innerText = data.code;
+    rate.innerText = data.rate;
+
+    btnDelete.addEventListener('click', deleteItem)
+
+    itemRef.classList.remove('hidden');
+    listRef.appendChild(itemRef);
+};
+
+const showResults = () => {
+  const result = getData('http://localhost:3000/currencies');
+  result.then((r) => {
+    r.forEach((item) => {
+      console.log(item);
+      createElement(item);
+    });
+  });
+};
+
+showResults();
+
+const getExchangeRate = async (code) => {
+  const url = 'http://api.nbp.pl/api/exchangerates/rates/a/';
+  return await fetch(`${url}${code}/`)
+    .then((response) => {
+      if (response.status == 200) {
+        return response.json();
+      }
+      return response.text();
+    })
+    .catch((e) => e);
+};
+
+// const result = await getExchangeRate("hrk");
+// console.log(result);
+
+// btnCheckCurrencyRef.addEventListener("click", function(event){
+//   event.preventDefault();
+//   console.log(this);
+// });
 
 btnCheckCurrencyRef.addEventListener('click', (event) => {
-    // wylaczenie zachowan przegladarki
-    event.preventDefault();
-    alertMessageRef.classList.add('hidden-anim');
-    
-    
-    const code = currencyCodesRef.value
+  event.preventDefault();
 
-    const result = getExchangeRate(code)
+  const code = currencyCodesRef.value;
 
-    const cardResult = cardRef.cloneNode(true)
-    cardResult.classList.remove("hidden")
-    
-    const cardHeader = cardResult.querySelector(".card-header");
-    const cardTitle = cardResult.querySelector('.card-title');
-    const cardText = cardResult.querySelector('.card-text');
-    
-    result.then(data => {
-        if (typeof data === 'string') {
-            alertMessageRef.innerText = `${code} is not found`
-            alertMessageRef.classList.remove("hidden-anim")
-        } else {
-            cardHeader.innerText = data.code;
-            cardTitle.innerText = data.currency.capitalize();
-            cardText.innerText = data.rates[0].mid
-            resultRef.prepend(cardResult);
-        }
-    })
+  const result = getExchangeRate(code);
+
+  const cardResult = cardRef.cloneNode(true);
+  cardResult.classList.remove('hidden');
+
+  const cardHeader = cardResult.querySelector('.card-header');
+  const cardTitle = cardResult.querySelector('.card-title');
+  const cardText = cardResult.querySelector('.card-text');
+
+  result.then((data) => {
+    if (typeof data === 'string') {
+      alertMessageRef.innerText = `${code} is not found. Try another currency.`;
+      alertMessageRef.classList.remove('hidden-anim');
+    } else {
+      alertMessageRef.classList.add('hidden-anim');
+      cardHeader.innerText = data.code;
+      cardText.innerText = data.rates[0].mid;
+      cardTitle.innerText = data.currency.capitalize();
+      resultRef.prepend(cardResult);
+
+      const db_currency = {
+        code: data.code,
+        name: data.currency,
+        rate: data.rates[0].mid,
+        date: new Date(),
+      };
+
+      postData('http://localhost:3000/currencies', db_currency);
+    }
+  });
 });
